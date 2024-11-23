b48e25e0c38ec6cd6ac80771143aa494522b3743
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2024-11-23 07:55:35 +0700

    feat(file): add upload-message endpoint and support for message attachments

--- src/modules/file/file.service.ts
+++ src/modules/file/file.service.ts
@@ -5,12 +5,18 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { v4 as uuid } from 'uuid';
 import { UploadFileDto, UploadFileResponseDto } from './dto/upload-file.dto';
+import { MessageAttachment } from '@/entities/message-attachment.entity';
+import { GroupMessageAttachment } from '@/entities/group-message-attachment.entity';
 
 @Injectable()
 export class FileService {
   constructor(
     @InjectRepository(File)
     private readonly fileRepository: Repository<File>,
+    @InjectRepository(MessageAttachment)
+    private readonly messageAttachmentRepository: Repository<MessageAttachment>,
+    @InjectRepository(GroupMessageAttachment)
+    private readonly groupMessageAttachmentRepository: Repository<GroupMessageAttachment>,
   ) {}
 
   private readonly s3Client = new S3Client({
@@ -20,6 +26,67 @@ export class FileService {
       secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
     },
   });
+
+  async uploadFiles(
+    files: Express.Multer.File[],
+    data: UploadFileDto,
+  ): Promise<UploadFileResponseDto[]> {
+    const uploadPromises = files.map((file) => this.uploadMessage(file, data));
+    return Promise.all(uploadPromises);
+  }
+
+  private async uploadMessage(
+    file: Express.Multer.File,
+    data: UploadFileDto,
+  ): Promise<UploadFileResponseDto> {
+    const { type } = data;
+    const key = `${Date.now().toString()}-${file.originalname}`;
+
+    // Upload file to S3
+    const putCommand = new PutObjectCommand({
+      Bucket: process.env.AWS_S3_BUCKET,
+      Key: key,
+      Body: file.buffer,
+      ContentType: file.mimetype,
+    });
+
+    try {
+      await this.s3Client.send(putCommand);
+    } catch (error) {
+      console.log('Error uploading file to S3', error);
+      throw new InternalServerErrorException();
+    }
+
+    let newFile;
+    if (type === 'message') {
+      newFile = await this.messageAttachmentRepository.save({
+        id: uuid(),
+        name: file.originalname,
+        key,
+        mimetype: file.mimetype,
+        url: `${process.env.CLOUDFRONT_URL}/${key}`,
+      });
+    } else if (type === 'group-message') {
+      newFile = await this.groupMessageAttachmentRepository.save({
+        id: uuid(),
+        name: file.originalname,
+        key,
+        mimetype: file.mimetype,
+        url: `${process.env.CLOUDFRONT_URL}/${key}`,
+      });
+    }
+
+    if (!newFile) {
+      throw new InternalServerErrorException('Error saving file to database');
+    }
+
+    return {
+      id: newFile.id,
+      url: newFile.url,
+      name: newFile.name,
+      mimetype: newFile.mimetype,
+    };
+  }
 
   async upload(
     file: Express.Multer.File,
